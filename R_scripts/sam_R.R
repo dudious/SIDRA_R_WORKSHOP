62ba0b1391401f6e97ee56cc9078c233aa66e3cd
BerniceQ <blo@sidra.org>
2016-05-19 14:05:35 +0300

    update samr

--- R_scripts/sam_R.R
+++ R_scripts/sam_R.R
@@ -1,5 +1,12 @@
 ### Statistical analysis of microarray
 ## load package
+
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("impute")
+
+
+install.packages("samr")
 library(samr)
 
 ### create data matrix
